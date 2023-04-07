cbbebc1f3864463c0be52ef7ed024ab61d6c909a
giuseppecapasso <capasso.gc@gmail.com>
2023-04-07 14:37:16 +0200

    add dark toggle

--- footer.php
+++ footer.php
@@ -1,17 +1,18 @@
 
 </main>
 
-<?php do_action( 'tailpress_content_end' ); ?>
+<?php do_action('tailpress_content_end'); ?>
 
 </div>
 
-<?php do_action( 'tailpress_content_after' ); ?>
+<?php do_action('tailpress_content_after'); ?>
 
-<footer id="colophon" class="site-footer bg-gray-50 py-12" role="contentinfo">
-	<?php do_action( 'tailpress_footer' ); ?>
+<footer id="colophon" class="site-footer bg-gray-50 py-12 dark:text-slate-400 dark:bg-slate-900" role="contentinfo">
+	
+	<?php do_action('tailpress_footer'); ?>
 
 	<div class="container mx-auto text-center text-gray-500">
-		&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?>
+		&copy; <?php echo date_i18n('Y');?> - <?php echo get_bloginfo('name');?>
 	</div>
 </footer>
 
