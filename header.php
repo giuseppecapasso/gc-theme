1395c2d5e49217169b73ceb9efb24c0b9d2f6ff9
giuseppecapasso <capasso.gc@gmail.com>
2024-01-03 16:25:03 +0100

    move toggledark

--- header.php
+++ header.php
@@ -16,96 +16,64 @@
 
 	<?php do_action('tailpress_header'); ?>
 
-	<!-- drawer component -->
-	<div id="drawer-example" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-80 dark:bg-gray-800" tabindex="-1" aria-labelledby="drawer-label">
-		<h5 id="drawer-label" class="inline-flex items-center mb-4 text-base font-semibold text-gray-500 dark:text-gray-400"><svg class="w-5 h-5 mr-2" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>Info</h5>
-		<button type="button" data-drawer-hide="drawer-example" aria-controls="drawer-example" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
-			<svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-			<span class="sr-only">Close menu</span>
-		</button>
-		<p class="mb-6 text-sm text-gray-500 dark:text-gray-400">Supercharge your hiring by taking advantage of our <a href="#" class="text-blue-600 underline dark:text-blue-500 hover:no-underline">limited-time sale</a> for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design job board.</p>
-		<div class="grid grid-cols-2 gap-4">
-			<a href="#" class="px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-lg focus:outline-none hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Learn more</a>
-			<a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get access <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>
+	<?php get_template_part('template-parts/layouts/drawer'); ?>
+
+	<header class="bg-opacity-50 backdrop-blur-sm sticky top-0 z-10 w-full bg-white border-b border-neutral-200 dark:bg-slate-900 dark:bg-opacity-80 dark:border-slate-700">
+		<!-- LOADING BAR -->
+		<div class="h-0.5 w-full bg-neutral-200 dark:bg-slate-900">
+			<div class="h-0.5 bg-primary" style="width: 50%"></div>
 		</div>
-	</div>
+		<!-- FINE LOADING BAR -->
 
-	<header class="sticky top-0 z-30 w-full bg-white dark:bg-slate-900 shadow-xl shadow-blue-600/20 ">
-		<button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" type="button" data-drawer-target="drawer-example" data-drawer-show="drawer-example" aria-controls="drawer-example">
-			Show drawer
-		</button>
+		<div class="xs:px-5 sm:px-4">
+			<div class="flex mx-auto max-w-8xl">
+				<div class="flex justify-between w-full">
+					<button class="lg:hidden p-3 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg" id="primary-menu-toggle" data-drawer-target="main-drawer" data-drawer-show="main-drawer" aria-controls="main-drawer"	>
+						<svg viewBox="0 0 20 20" class="w-5 h-5" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+							<g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
+								<g id="icon-shape">
+									<path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z"></path>
+								</g>
+							</g>
+						</svg>
+					</button>
 
-		<div class="px-2 py-2 sm:px-4">
-			<div class="flex items-center justify-between mx-auto max-w-8xl">
-				<div>
 					<?php if (has_custom_logo()) { ?>
 						<?php the_custom_logo(); ?>
 						<?php } else { ?>
-							<a href="<?php echo get_bloginfo('url'); ?>" class="text-2xl font-extrabold text-blue-600 dark:text-white">
-								<?php echo get_bloginfo('name'); ?>
-							</a>
-							<p class="text-sm font-light text-gray-600">
-								<?php echo get_bloginfo('description'); ?>
-							</p>
-							<?php } ?>
-						</div>
+							<div>
+								<a href="<?php echo get_bloginfo('url'); ?>" class="text-xl font-extrabold text-blue-600 dark:text-white">
+									<?php echo get_bloginfo('name'); ?>
+								</a>
+								<p class="text-sm font-light text-gray-600">
+									<?php echo get_bloginfo('description'); ?>
+								</p>
+							</div>
+						<?php } ?>		
 
-				<?php get_template_part('template-parts/layouts/darkToggle'); ?>
-
-				<div class="flex items-center space-x-1">
-					<ul class="hidden space-x-2 lg:inline-flex">
-						<?php
-                                            wp_nav_menu([
-                                                            'container_id'    => 'primary-menu',
-                                                            'container_class' => 'hidden space-x-2 md:inline-flex',
-                                                            'menu_class'      => 'lg:flex lg:-mx-4',
-                                                            'theme_location'  => 'primary',
-                                                            'li_class'        => 'px-4 py-2 font-semibold rounded',
-                                                            'fallback_cb'     => false,
-                                            ]);
-?>
-
-					</ul>
-					<div class="inline-flex lg:hidden">
-						<button class="flex-none px-2" aria-label="Toggle navigation" id="primary-menu-toggle">
-							<svg viewBox="0 0 20 20" class="inline-block w-6 h-6" version="1.1"
-									xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-								<g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
-									<g id="icon-shape">
-										<path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z"
-												id="Combined-Shape"></path>
-									</g>
-								</g>
-							</svg>
-							<span class="sr-only">Open Menu</span>
-						</button>
-						<!-- <div class="absolute top-0 left-0 right-0 z-50 flex flex-col p-2 pb-4 m-2 space-y-3 bg-white rounded shadow" x-show.transition="open">
-							<button class="self-end flex-none px-2 ml-2">
-								<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-								</svg>
-								<span class="sr-only">Close Menu</span>
-							</button>
-							<ul class="space-y-2">
-								<?php
-                                        wp_nav_menu([
-                                                                                            'container_id'    => 'primary-menu',
-                                                                                            'container_class' => '',
-                                                                                            'menu_class'      => 'lg:flex lg:-mx-4',
-                                                                                            'theme_location'  => 'primary',
-                                                                                            'li_class'        => 'px-4 py-2 font-semibold rounded',
-                                                                                            'fallback_cb'     => false,
-                                        ]);
-?>
-							</ul>
-						</div> -->
+					<!-- MENU PRINCIPALE -->
+					<div class="hidden items-center lg:inline-flex">
+						<!-- <ul class="space-x-2">
+							<?php wp_nav_menu(['container_id' => 'primary-menu','menu_class' => 'lg:flex lg:-mx-4','theme_location' => 'primary','li_class' => 'px-4 py-2 font-semibold rounded','fallback_cb' => false]); ?>
+						</ul> -->
+						<?php get_template_part('template-parts/layouts/mainMenu'); ?>
 					</div>
+					<!-- FINE MENU PRINCIPALE -->
+					
+					<button data-dropdown-toggle="dropDownMenu" class="p-3 lg:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg" type="button"> 
+						<svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"></path></svg>
+					</button>
 				</div>
 			</div>
 		</div>
-		<div class="sticky h-1 w-full bg-neutral-200 dark:bg-neutral-600">
-			<div class="h-1 bg-primary" style="width: 50%"></div>
-		</div>
+
+		<?php get_template_part('template-parts/layouts/dropdownMenu'); ?>
+
+		<?php if (is_single()) { ?>
+			<div class="h-0.5 w-full bg-white bg-opacity-50 backdrop-blur-sm dark:bg-slate-900">
+				<div class="h-0.5 bg-primary" id="progressbar" style="width: 0%"></div>
+			</div>
+		<?php } ?>
 	</header>
 
 	<div id="content" class="site-content flex-grow">
@@ -113,21 +81,27 @@
 			<!-- Start introduction -->
 			<div class="container mx-auto">
 				<div class="px-12 py-16 my-12 rounded-xl bg-gradient-to-r from-blue-50 from-10% via-sky-100 via-30% to-blue-200 to-90%">
-						<div class="mx-auto max-w-screen-md">
-								<h1 class="text-3xl lg:text-6xl tracking-tight font-extrabold text-gray-800 mb-6">Start building your next <a href="https://tailwindcss.com" class="text-secondary">Tailwind CSS</a> flavoured WordPress theme
-										with <a href="https://tailpress.io" class="text-primary">TailPress</a>.</h1>
-								<p class="text-gray-600 text-xl font-medium mb-10">TailPress is your go-to starting
-										point for developing WordPress themes with Tailwind CSS and comes with basic block-editor support out
-										of the box.</p>
-								<a href="https://github.com/jeffreyvr/tailpress"
-										class="w-full sm:w-auto flex-none bg-gray-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">View
-										on GitHub</a>
-						</div>
-				</div>
+					<div class="mx-auto max-w-screen-md">
+							<h1 class="text-3xl lg:text-6xl tracking-tight font-extrabold text-gray-800 mb-6">Start building your next <a href="https://tailwindcss.com" class="text-secondary">Tailwind CSS</a> flavoured WordPress theme
+									with <a href="https://tailpress.io" class="text-primary">TailPress</a>.</h1>
+							<p class="text-gray-600 text-xl font-medium mb-10">TailPress is your go-to starting
+									point for developing WordPress themes with Tailwind CSS and comes with basic block-editor support out
+									of the box.</p>
+							<a href="https://github.com/jeffreyvr/tailpress"
+									class="w-full sm:w-auto flex-none bg-gray-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">View
+									on GitHub</a>
+					</div>
 			</div>
-			<!-- End introduction -->
-		<?php } ?>
+		</div>
+		<!-- End introduction -->
+	<?php } ?>
 
-		<?php do_action('tailpress_content_start'); ?>
+	<?php do_action('tailpress_content_start'); ?>
 
-		<main>
+	<main>
+
+	<script>
+		document.addEventListener("scroll", function() {
+			document.getElementById("progressbar").style = `width: ${window.scrollY / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100}%`
+		})
+	</script>
--- template-parts/layouts/darkToggle.php
+++ template-parts/layouts/darkToggle.php
@@ -1,5 +1,5 @@
-<button id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 rounded-lg p-2" >
-  <svg id="iconLight" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" class="w-5 fill-slate-500 hidden">
+<button id="theme-toggle" type="button" class="hidden lg:inline-flex" >
+  <svg id="iconLight" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" class="w-5 fill-gray-300 hover:fill-white hidden">
     <rect x="15" y="2" width="2" height="5"/>
     <rect x="21.6675" y="6.8536" width="4.958" height="1.9998" transform="translate(1.5191 19.3744) rotate(-45)"/>
     <rect x="25" y="15" width="5" height="2"/>
@@ -11,7 +11,7 @@
     <path d="M16,12a4,4,0,1,1-4,4,4.0045,4.0045,0,0,1,4-4m0-2a6,6,0,1,0,6,6,6,6,0,0,0-6-6Z"/>
   </svg>
 
-  <svg id="iconDark" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" class="w-5 fill-slate-500 hidden">
+  <svg id="iconDark" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" class="w-5 fill-gray-300 hover:fill-gray-900 hidden">
     <path d="M27,18a.9986.9986,0,0,1-.5415-.1594L22,14.9688l-4.4585,2.8718a1,1,0,0,1-1.5088-1.095l1.35-5.1318L14.3105,8.69a1,1,0,0,1,.5508-1.7146l4.3745-.6128,1.8663-3.8032A1.0008,1.0008,0,0,1,21.9878,2a1.0171,1.0171,0,0,1,.8989.5378L24.88,6.3625l4.2622.6133A1,1,0,0,1,29.69,8.69l-3.0723,2.9239,1.35,5.1318A1,1,0,0,1,27,18Zm-5-5.2207a.999.999,0,0,1,.5415.1594l2.8369,1.8272-.8457-3.2149a1.0006,1.0006,0,0,1,.2778-.9787l2.0074-1.91-2.73-.3929a.9994.9994,0,0,1-.7441-.5275L22.0269,5.2151,20.7979,7.72a1.0008,1.0008,0,0,1-.7593.55L17.19,8.6687l2,1.9036a1.0006,1.0006,0,0,1,.2778.9787l-.8457,3.2149,2.8369-1.8272A.999.999,0,0,1,22,12.7793Z"/>
     <path d="M15.8169,30A13.8137,13.8137,0,0,1,9.0752,4.1277a1,1,0,0,1,1.3613,1.3611A11.8091,11.8091,0,0,0,26.5107,21.5635a1,1,0,0,1,1.3619,1.3611A13.8411,13.8411,0,0,1,15.8169,30ZM7.3271,7.9727a11.81,11.81,0,0,0,16.7,16.7,13.8086,13.8086,0,0,1-16.7-16.7Z"/>
   </svg>
